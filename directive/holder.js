8936aad4bd8e1d6c7ff6b5a26b785259b4ce7b7c
Michal Sedlák <sedlakmichal@gmail.com>
2015-03-03 11:12:47 +0100

    Fix treebank browse

--- directive/holder.js
+++ directive/holder.js
@@ -2,9 +2,10 @@ angular.module('pmltqWeb').directive('holder', function() {
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
-        if(attrs.holder)
-          attrs.$set('data-src', attrs.holder);
-        Holder.run({images:element[0]});
+      if(attrs.holder) {
+        attrs.$set('data-src', attrs.holder);
       }
+      Holder.run({images:element[0]});
+    }
   };
 });
