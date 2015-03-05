23fa15621b64493eae7b1c52b1c53a7cae3af52f
Michal Sedlák <sedlakmichal@gmail.com>
2015-03-05 11:38:51 +0100

    Add filter and remove holder

---
+++ filter/treebankFilter-spec.js
@@ -0,0 +1,13 @@
+describe('treebankFilter', function() {
+
+	beforeEach(module('pmltqWeb'));
+
+	it('should ...', inject(function($filter) {
+
+    var filter = $filter('treebankFilter');
+
+		expect(filter('input')).toEqual('input');
+
+	}));
+
+});
