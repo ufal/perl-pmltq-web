8b69fd4f1f05c78f7f8e01756f2498906137ad23
Michal Sedlák <sedlakmichal@gmail.com>
2015-03-18 14:46:41 +0100

    Build a table if result is a table

--- pmltqTreebank/directive/resultTable.js
+++ pmltqTreebank/directive/resultTable.js
@@ -1,21 +1,33 @@
 angular.module('pmltqTreebank').directive('resultTable', function() {
+  function buildTable(dataTable) {
+    var table = $('<table></table>').addClass('table');
+    for (var row = 0; row < dataTable.length; row++) {
+      var tr = $('<tr/>');
+      table.append(tr);
+      for (var col = 0; col < dataTable[row].length; col++) {
+        var td = $('<td/>').text(dataTable[row][col]);
+        tr.append(td);
+      }
+    }
+
+    return table;
+  }
+
   return {
     restrict: 'A',
-    require: '^treebankDetail',
+    scope: {
+      table: '=resultTable'
+    },
     link: function($scope, $element, $attrs) {
-      $scope.$on('result.table', function(result, resultNo, results, ctrl) {
-        var table = $('<table></table>');
-        for (var row = 0; row < results.length; row++) {
-          var tr = $('<tr/>');
-          table.append(tr);
-          for (var col = 0; col < results[row].length; col++) {
-            var td = $('<td/>').text(results[row][col]);
-            tr.append(td);
-          }
-        }
-        $element.empty().append(table);
-      });
+      var lastTable;
 
+      $scope.$watch('table', function(table) {
+        if (angular.equals(table, lastTable)) {
+          return;
+        }
+        lastTable = table;
+        $element.empty().append(buildTable(table));
+      });
     }
   };
 });
