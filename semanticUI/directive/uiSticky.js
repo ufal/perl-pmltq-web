9335e31366ab52c890c2e8bcde596c2feaa2cd43
Michal Sedlák <sedlakmichal@gmail.com>
2015-04-10 18:00:07 +0200

    Restyled help page

---
+++ semanticUI/directive/uiSticky.js
@@ -0,0 +1,16 @@
+angular.module('semanticUI').directive('uiSticky', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      options: '=?uiSticky'
+    },
+    link: function($scope, $element, $attrs) {
+      $scope.$watch('options', function(options) {
+        if (angular.isUndefined(options) || options === $attrs['uiSticky']) {
+          options = {};
+        }
+        $($element).sticky(options);
+      });
+    }
+  };
+});
