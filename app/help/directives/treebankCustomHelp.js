3f6af6a83a569aa4f1cc3ade5461176a374e9d94
Matyáš Kopp <matyas.kopp@gmail.com>
2017-02-22 11:53:22 +0100

    added syntax highlighter and try button to help change treebank-custom-help from element to class

--- app/help/directives/treebankCustomHelp.js
+++ app/help/directives/treebankCustomHelp.js
@@ -5,7 +5,10 @@ var md = require('markdown-it')({
   modifyToken: function (token, env) {
     switch (token.type) {
     case 'fence':
-      token.tag = 'query-example';
+      token.tag = '';
+      token.markup = '';
+      token.type = 'html_block';
+      token.content = '<query-example treebank="treebank"><pre>'+token.content+'</pre></query-example>';
       break;
     case 'link_open':
       token.attrObj.target = '_blank'; // set all links to open in new window
@@ -14,19 +17,18 @@ var md = require('markdown-it')({
   }
 }).use(require('markdown-it-modify-token'));;
 
-module.exports = function() {
+module.exports = function($compile) {
   'ngInject';
 
   return {
-    restrict: 'E',
+    restrict: 'EC',
     replace: true,
     scope: {
       treebank: '='
     },
-    link: function(scope, element, attrs) {
-            var res = md.render(scope.treebank.documentation);
-            console.log(element);
-            element.html(res);
-        }
+    link: function($scope, $element, $attr) {
+      var res = md.render($scope.treebank.documentation);
+      $element.html($compile(res)($scope));
+    }
   };
 };
