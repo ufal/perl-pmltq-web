1ca5d6d94f3ea5a21d5ecfc895180129dfe1c4cc
Matyáš Kopp <matyas.kopp@gmail.com>
2017-04-13 22:37:57 +0200

    add queryRecordId when query evaluating

--- app/query/queryParams.js
+++ app/query/queryParams.js
@@ -39,6 +39,7 @@ module.exports = function QueryParamsFactory($state, localStorageService, rx) {
       this._limit = limit > 0 ? limit : 100;
       this._timeout = timeout > 0 ? timeout : 30;
       this._filter = angular.isDefined(filter) ? filter : true;
+      this._queryRecordId = undefined;
       this._oldParams = this.params();
 
       this.suggest = new rx.ReplaySubject(1);
@@ -80,6 +81,15 @@ module.exports = function QueryParamsFactory($state, localStorageService, rx) {
       this.cache();
     }
 
+    get queryRecordId() {
+      return this._queryRecordId;
+    }
+
+    set queryRecordId(queryRecordId) {
+      this._queryRecordId = queryRecordId;
+      this.cache();
+    }
+
     clear() {
       this._query = '';
       this._filter = false;
@@ -109,7 +119,8 @@ module.exports = function QueryParamsFactory($state, localStorageService, rx) {
         filter: this.filter,
         limit: this.limit,
         query: this.query,
-        timeout: this.timeout
+        timeout: this.timeout,
+        queryRecordId: this.queryRecordId
       };
     } 
   }
